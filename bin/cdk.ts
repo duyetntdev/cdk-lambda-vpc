e4fd62a3aad202516324802980892bd54c6d8359
duyetntdev <117375146+duyetntdev@users.noreply.github.com>
2022-12-20 20:13:58 +0700

    Update cdk.ts

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -6,5 +6,5 @@ import { CdkStack } from '../lib/cdk-stack';
 const app = new cdk.App();
 
 new CdkStack(app, 'CdkDemoStack', {
-   env: { account: '831344450728', region: 'ap-northeast-1' },
+   env: { account: '123456789', region: 'ap-northeast-1' },
 });
